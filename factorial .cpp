39b8c44523c778cb7bd02824ca8c5f016101eb74
venu6964 <124232116+venu6964@users.noreply.github.com>
2023-10-16 12:29:42 +0530

    Add files via upload

---
+++ factorial .cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	int i,n,fact=1;
+	printf("enter the number");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		fact=fact*i;
+	}
+	printf("enter the numner%d:%d",n,fact);
+	return 0;
+}
