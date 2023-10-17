32283da4ece25f9b3f56a24a156ef24e361e5c04
venu6964 <124232116+venu6964@users.noreply.github.com>
2023-10-17 08:13:47 +0530

    Add files via upload

---
+++ fibo recursion.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+ int Fibonacci(int n );
+int main()
+{
+   int n, i = 0, c;
+   printf("enter the number");
+   scanf("%d",&n);
+   printf("Fibonacci series\n");
+ 
+   for ( c = 1 ; c <= n ; c++ )
+   {
+      printf("%d\n", Fibonacci(i));
+      i++; 
+   }
+    
+}
+ 
+int Fibonacci(int n)
+{
+   if ( n == 0 )
+      return 0;
+   else if ( n == 1 )
+      return 1;
+   else
+      return ( Fibonacci(n-1) + Fibonacci(n-2) );
+}
